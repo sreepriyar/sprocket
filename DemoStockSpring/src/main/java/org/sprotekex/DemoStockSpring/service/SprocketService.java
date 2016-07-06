7c1a1da62356121b37d96776f73de08a215a6b22
sreepriya <sreepriyaganesh@gmail.com>
2016-07-06 14:30:21 -0700

    updated

--- DemoStockSpring/src/main/java/org/sprotekex/DemoStockSpring/service/SprocketService.java
+++
@@ -1,9 +0,0 @@
-package org.sprotekex.DemoStockSpring.service;
-
-import org.sprotekex.DemoStockSpring.model.UserSprocket;
-
-public interface SprocketService {
-	
-	UserSprocket getSprocketsByUser(String userId);
-
-}
